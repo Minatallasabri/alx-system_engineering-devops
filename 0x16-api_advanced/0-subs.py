a9d1cfa2f5d3892cf86d371fcafa8c87f863e725
Minatallasabri <minatallasabrri3380@gmail.com>
2024-08-23 07:21:49 +0300

    sikildim_Task0_Task1 at 23-07

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of subscribers
-for a given subreddit. If not valid subreddit, returns 0
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    try:
-        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)\
-            AppleWebKit/537.36 (KHTML, like Gecko)\
-            Chrome/39.0.2171.95 Safari/537.36',
-            'accept': 'application/json'}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Check if the request was successful
-        data = response.json()
-        
-        # Check if 'data' and 'subscribers' are in the response
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
-    except KeyError:
-        return 0
 
-# Example usage
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    print(number_of_subscribers(subreddit))
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
