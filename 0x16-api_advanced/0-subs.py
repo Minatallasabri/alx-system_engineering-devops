c805040fc488d1f198e318cde61a561dfa1179d5
Minatallasabri <minatallasabrri3380@gmail.com>
2024-08-23 06:49:07 +0300

    tring_Task0 at 23-06

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+Queries the Reddit API and returns the number of subscribers
+for a given subreddit. If not valid subreddit, returns 0
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-
-    headers = {
-        'User-Agent': u_agent
-    }
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    try:
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)\
+            AppleWebKit/537.36 (KHTML, like Gecko)\
+            Chrome/39.0.2171.95 Safari/537.36',
+            'accept': 'application/json'}
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False).json()
+        return response['data']['subscribers']
+    except BaseException:
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
